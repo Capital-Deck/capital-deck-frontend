9f50f8d0673053c20716c79eb6f0b2d8bfbb9316
Salman Zafar <18salmanz@gmail.com>
2023-01-26 12:24:53 +0530

    add capital-deck frontend

---
+++ src/util/BaseUrl.js
@@ -0,0 +1,10 @@
+// export const baseURL = 'http://localhost:80/api';
+// export const baseUploadURL = 'http://localhost:80/';
+
+//! For Render Deployment
+export const baseURL = 'https://capital-deck.onrender.com/api';
+export const baseUploadURL = 'https://capital-deck.onrender.com/';
+
+// //! For Vercel Deployment
+// export const baseURL = 'https://capital-deck-backend.vercel.app/api';
+// export const baseUploadURL = 'https://capital-deck-backend.vercel.app/';
